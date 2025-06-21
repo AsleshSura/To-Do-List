ca68e61e16fe57558b5ae80dbda166ceba432afd
Aslesh Sura <82917526+AsleshSura@users.noreply.github.com>
2025-06-21 16:40:27 -0500

    Base Ideas

---
+++ main.py
@@ -0,0 +1,9 @@
+#OMSAIRAM
+
+"""
+Template:
+- A to-do list that lets people add, remove, and view tasks.
+- Should be able to send notifications for tasks that are due today.
+- Uses a simple text file to store tasks.
+- Uses Python's built-in libraries.
+"""
